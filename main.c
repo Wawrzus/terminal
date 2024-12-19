78fb03076731989d270085f4e92b62f5658263c1
Szymon <wawrzyczekszymon@gmail.com>
2024-12-19 11:48:17 +0100

    create help command and clear command handler

--- main.c
+++ main.c
@@ -1,14 +1,50 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <dirent.h>
+
+void clearScreen()
+{
+    system("clear");
+}
+
+void help()
+{
+    printf("---------- HELP ----------\n");
+    printf("1. exit-terminal >> zamkniecie konsoli\n");
+    printf("2. help >> wyswietlenie opisu komend\n");
+}
+
+int checkCommand(char *command)
+{
+    const char commandArray[][200] = {"exit-terminal", "help", "clear"};
+    for(int i = 0; i < sizeof(commandArray)/sizeof(commandArray[0]); i++)
+    {
+        if(strcmp(commandArray[i], command) == 0)
+            return i;
+    }
+}
 
 void mainLoop()
 {
-    char choseCommand[100];
-    while (strcmp("exit-terminal", choseCommand) != 0)
+    char command[200];
+    while (strcmp("exit-terminal", command) != 0)
     {
-        printf("%s", "Wpisz komende: ");
-        scanf("%s", choseCommand);
+        printf("$: ");
+        scanf("%s", command);
+        switch (checkCommand(command))
+        {
+        case 0:
+            break;
+        case 1:
+            help();
+            break;
+        case 2:
+            clearScreen();
+            break;
+        default:
+            break;
+        }
     }
 }
 
