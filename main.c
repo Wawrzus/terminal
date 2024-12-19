55286d455ab524aeb4773d097e210534c437d20f
Szymon <wawrzyczekszymon@gmail.com>
2024-12-19 16:53:56 +0100

    create change directory command handler

--- main.c
+++ main.c
@@ -3,14 +3,28 @@
 #include <string.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <string.h>
 
 #define PATH_SIZE 1024
+#define COMMAND_SIZE 200
 
+//change working directory
 void changeDirectory()
 {
-    
+    char dirName[COMMAND_SIZE];
+    printf("$:dirname: ");
+    fgets(dirName, sizeof(dirName), stdin);
+    for (int i = 0; i < (int)strlen(dirName); i++)
+    {
+        if(dirName[i] == '\n')
+            dirName[i] = '\0'; 
+        else
+            dirName[i] = dirName[i];
+    }
+    chdir(dirName);
 }
 
+//list content of current directory
 void listFiles()
 {
     struct dirent *direntPointer;
@@ -21,6 +35,7 @@ void listFiles()
     closedir(dir);
 }
 
+//check current direcotry path
 void currentDirectory()
 {
     char directory[PATH_SIZE];
@@ -40,15 +55,23 @@ void help()
     printf("exit-terminal >> close terminal\n");
     printf("help >> show commands description\n");
     printf("clear >> clear terminal\n");
-    printf("list-files >> list all files and directories in current directory\n");
+    printf("list-files >> list current directory content\n");
     printf("current-dir >> show current path\n");
+    printf("change-dir >> change working directory\n");
 }
 
 //check what command user used
 int checkCommand(char *command)
 {
+    for (int i = 0; i < (int)strlen(command); i++)
+    {
+        if(command[i] == '\n')
+            command[i] = '\0'; 
+        else
+            command[i] = command[i];
+    }
     int commandIndex = 0;
-    const char commandArray[][200] = {"exit-terminal", "help", "clear", "list-files", "current-dir"}; 
+    const char commandArray[][200] = {"exit-terminal", "help", "clear", "list-files", "current-dir", "change-dir"}; 
     for(int i = 0; i < (int)(sizeof(commandArray)/sizeof(commandArray[0])); i++)
     {
         if(strcmp(commandArray[i], command) == 0)
@@ -59,11 +82,13 @@ int checkCommand(char *command)
 
 void mainLoop()
 {
-    char command[200];
+    char command[COMMAND_SIZE];
     while (strcmp("exit-terminal", command) != 0)
     {
-        printf("$: ");
-        scanf("%s", command);
+        char directory[PATH_SIZE];
+        printf("%s $: ", getcwd(directory, PATH_SIZE));
+        fgets(command, sizeof(command), stdin);
+        checkCommand(command);
         switch (checkCommand(command))
         {
         case 0:
@@ -80,6 +105,9 @@ void mainLoop()
         case 4:
             currentDirectory();
             break;
+        case 5:
+            changeDirectory();
+            break;
         default:
             break;
         }
@@ -89,4 +117,5 @@ void mainLoop()
 int main(void)
 {
     mainLoop();
+    return 0;
 }
